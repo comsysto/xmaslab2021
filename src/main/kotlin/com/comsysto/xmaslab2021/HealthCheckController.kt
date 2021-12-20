6debdaf07ea42b0b586fb27cbd0b0a5d7d22c090
iponomarev <ivan.ponomarev@comsysto.com>
2021-12-20 11:40:02 +0100

    Add heath check

---
+++ src/main/kotlin/com/comsysto/xmaslab2021/HealthCheckController.kt
@@ -0,0 +1,18 @@
+package com.comsysto.xmaslab2021
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+
+    companion object {
+        const val HEALTHY_MESSAGE = "I am healthy!"
+    }
+
+    @GetMapping("/healthCheck")
+    fun healthCheck(): String {
+        return HEALTHY_MESSAGE
+    }
+
+}
