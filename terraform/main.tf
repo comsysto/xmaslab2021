5dc20f0296a642afec0160564cfb0a67bcc93682
Ahmad Hasan <62647936+AhmadHasanAtReplyDe@users.noreply.github.com>
2021-12-22 14:53:07 +0100

    Terraform Pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,12 +16,12 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "xlabrg" {
-  name     = "XmasLab2021"
+  name     = "XmasLab2021DingsBums"
   location = "West Europe"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "XmasLabCR"
+  name                = "XmasLabCRDingsBums"
   resource_group_name = azurerm_resource_group.xlabrg.name
   location            = azurerm_resource_group.xlabrg.location
   sku                 = "Standard"
@@ -29,36 +29,19 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = var.cluster_name
-  location            = azurerm_resource_group.k8s.location
-  resource_group_name = azurerm_resource_group.k8s.name
-  dns_prefix          = var.dns_prefix
+  name                = "XMasLab2021KS"
+  location            = azurerm_resource_group.xlabrg.location
+  resource_group_name = azurerm_resource_group.xlabrg.name
+  dns_prefix          = "xmas"
 
-  linux_profile {
-    admin_username = "ubuntu"
-
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
+  identity {
+    type = "SystemAssigned"
   }
-
 
   default_node_pool {
     name       = "agentpool"
-    node_count = var.agent_count
+    node_count = 1
     vm_size    = "Standard_D2_v2"
-  }
-
-  service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
-  }
-
-  addon_profile {
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
-    }
   }
 
   network_profile {
